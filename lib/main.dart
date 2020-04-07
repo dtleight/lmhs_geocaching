185a0da6c46c7e2e69e6e1d02b35f7c9e7b33b62
dtleight <dtleight@gmail.com>
2020-04-06 22:21:45 -0400

    Removed redundant references, Added ListView
    Removed redundant pre-generated comments, implemented a ListView that will display data on nearby caches.

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: PageViewDemo()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: PageViewDemo());
   }
 }
 
@@ -73,10 +63,26 @@ class CachePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Text("Another Page...!!!!!!"),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Cache Title #1'),
+          ),
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Cache Title #2'),
+          ),
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Cache Title #3'),
+          ),
+        ],
+      ),
     );
   }
 }
+
 class PageViewDemo extends StatefulWidget {
   @override
   _PageViewDemoState createState() => _PageViewDemoState();
