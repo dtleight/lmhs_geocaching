7fe8f979be388dc230af6df8db0cbbbf32623735
dtleight <dtleight@gmail.com>
2020-04-11 23:25:45 -0400

    Refactored Code Base
    Refactored classes to move them into seperate files, should allow for easier workflow along with a slight efficiency gain.

---
+++ lib/PageContainer.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'CachePage.dart';
+import 'HomePage.dart';
+
+class PageContainer extends StatefulWidget {
+  @override
+  _PageViewDemoState createState() => _PageViewDemoState();
+}
+
+class _PageViewDemoState extends State<PageContainer> {
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      children: [
+        HomePage(title: 'Lower Macungie Historical Society Geocaching'),
+        CachePage(),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,219 +1,22 @@
-import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:lmhsgeocaching/PageContainer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: PageViewDemo());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  GoogleMapController mapController;
-  LatLng _pos = const LatLng(40.523938, -75.547719);
-  LocationData currentLocation;
-  Location location;
-
-
-  void _onMapCreated(GoogleMapController controller) async{
-    mapController = controller;
-  }
-  void _getLocation() async {
-    var location = new Location();
-    try {
-      currentLocation = await location.getLocation();
-      setState(
-              () {}); //rebuild the widget after getting the current location of the user
-    } on Exception {
-      currentLocation = null;
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _pos,
-            zoom: 15.0,
-          ),
-          myLocationEnabled: true,
-        ));
-  }
-}
-
-class CachePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Cache Title #1'),
-            onTap: (){ Navigator.push(context, new MaterialPageRoute(builder: (ctxt) => new CacheInfoPage()));} ,
-          ),
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Cache Title #2'),
-          ),
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Cache Title #3'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class PageViewDemo extends StatefulWidget {
-  @override
-  _PageViewDemoState createState() => _PageViewDemoState();
-}
-
-class _PageViewDemoState extends State<PageViewDemo> {
-  PageController _controller = PageController(
-    initialPage: 0,
-  );
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return PageView(
-      controller: _controller,
-      children: [
-        MyHomePage(title: 'Lower Macungie Historical Society Geocaching'),
-        CachePage(),
-      ],
-    );
-  }
-}
-
-class CacheInfoPage extends StatelessWidget {
-  GoogleMapController mapController;
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-  final String _cacheName = "Cache #1";
-  final String _cacheLoc = "Emmaus High School";
-  final LatLng _cacheLatLng = const LatLng(40.533940, -75.506032);
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> _cacheLatLngList = _cacheLatLng.toString().split(",");
-    String _cacheLat = _cacheLatLngList[0].substring(7, _cacheLatLngList[0].length);
-    String _cacheLng = _cacheLatLngList[1].substring(0, _cacheLatLngList[1].length - 1);
-    return new Scaffold(
-      appBar: AppBar(title: Text(_cacheName + " Information")),
-      body: ListView(
-        children: <Widget>[
-          Text(
-            "Location: " + _cacheLoc + "\nLat: " + _cacheLat + " Long: " + _cacheLng,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          SizedBox(
-            height: 300,
-            child: GoogleMap( //Map seems to take a long time to load in on my phone - Magee
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _cacheLatLng,
-                zoom: 15.0,
-              ),
-            ),
-          ),
-          // History
-          Text(
-            "History",
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          Text("This is an explanation of the historical significance of the location of Cache #1."),
-          // Instructions
-          Text(
-            "Instructions",
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          Text("These are the instructions to find Cache #1."),
-          // Comments
-          Text(
-            "Comments",
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          Card(
-            child:
-            Row(
-              children: <Widget>[
-                Icon(Icons.person),
-                Text("This is one possible format for comments."),
-              ]
-            ),
-          ),
-          Card(
-            child:
-            Row(
-                children: <Widget>[
-                  Icon(Icons.person),
-                  Text("Here are two more,"),
-                ]
-            ),
-          ),
-          Card(
-            child:
-            Row(
-                children: <Widget>[
-                  Icon(Icons.person),
-                  Text("so this can scroll."),
-                ]
-            ),
-          ),
-        ],
-      )
-    );
+        home: PageContainer());
   }
 }
