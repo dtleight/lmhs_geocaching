9124176f609be71ac2e112cea1ffce9c639f65b9
dtleight <dtleight@gmail.com>
2020-04-13 16:08:00 -0400

    Added UtilitySelector Page

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget
   Widget build(BuildContext context)
   {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'LMHS Geocaching',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
