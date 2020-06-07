b57839b79f8e2b27682b6982c7bcee9c9428587f
dtleight <dtleight@gmail.com>
2020-06-07 13:28:55 -0400

    Added CacheContainer
    CacheContainer is the active cache page controller.

---
+++ lib/CacheContainer.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'CachePage.dart';
+
+class CacheContainer extends StatefulWidget {
+  @override
+  _PageViewDemoState createState() => _PageViewDemoState();
+}
+
+class _PageViewDemoState extends State<CacheContainer> {
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      children: [
+        CachePage(),
+
+      ],
+    );
+  }
+}
