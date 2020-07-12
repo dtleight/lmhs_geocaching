323a9200db2a947e6da4688a5ec528fcfd0764f2
dtleight <dtleight@gmail.com>
2020-07-11 21:57:20 -0400

    General bug fixes

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,28 +1,36 @@
 import 'Badge.dart';
 class Profile {
+  String name;
+  String UUID;
+  DateTime joinDate;
+  List<int> cacheCompletions;
+  List<int> badgeCompletions;
+
+
+
+  Profile(String name, String UUID, DateTime joinDate, List<int> cacheCompletions, List<int> badgeCompletions)
+  {
+   this.name = name;
+   this.UUID = UUID;
+   this.joinDate = joinDate;
+   this.cacheCompletions = cacheCompletions;
+   this.badgeCompletions = badgeCompletions;
+  }
   //TODO: Write profile info to the database
   String sort='alpha';
-  /**
-  static List<Badge> badgeList = [
-    new Badge("School", "badge-images/school.jpg", 1),
-    new Badge("Church", "badge-images/church.jpg", 2),
-    new Badge("Barn", "badge-images/barn.jpg", 3)
-  ];
-      **/
-  Profile() {}
 /**
   //this should automatically run whenever the user goes to look at his/her badges
-  //for(var i=0; i<badgeList.length; i++){
+    //for(var i=0; i<badgeList.length; i++){
 
-  alphasort()  {
+    alphasort()  {
 
-  }
+    }
 
-  topicsort() {
+    topicsort() {
 
-  }
+    }
 
-  newestsort() {
+    newestsort() {
 
   }
     **/
--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,11 @@ class MyApp extends StatelessWidget
   {
     return MaterialApp(
         title: 'LMHS Geocaching',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: PageContainer());
+
   }
 }
