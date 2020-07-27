0c917a3f71bb378c270a49c15592ce3e77510a97
dhkreidler <dhkreidler@gmail.com>
2020-07-26 22:49:47 -0400

    Worked on CacheNotFoundPage.dart:     Added TextField     Added a Send Message Button     Added flutter_email_sender      Added variables related to the email function     The radio button that is clicked will be the subject of the email and the text typed into the TextField will be the body of the email     Goes to the user's default email app     User hits send
        //Still need to add something to reset the page without closing out of the app entirely

--- lib/CacheNotFoundPage.dart
+++ lib/CacheNotFoundPage.dart
@@ -2,14 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'Account.dart';
+String subject1;
+String body;
+
+final myController = TextEditingController();
 
 class CacheNotFoundPage extends StatefulWidget
 {
+
   @override
   _CacheNotFoundState createState() => new _CacheNotFoundState();
 }
 
 class _CacheNotFoundState extends State<CacheNotFoundPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +37,32 @@ class _CacheNotFoundState extends State<CacheNotFoundPage> {
             height: 25,
           ),
           OptionsNotFound(),
-        ],
-      ),
+          SizedBox(
+            height: 25,
+          ),
+        TextField(
+          controller: (myController),
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Please explain your issue in more detail.",
+          ),
+        ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 8.0),
+            child: RaisedButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              splashColor: Colors.blueGrey,
+              child: const Text('Send Message'),
+              onPressed: () {
+                subject1 = subject1.toString();
+                body = myController.text.toString();
+                Email();
+                FlutterEmailSender.send(mailer);
+              }
+            ),
+          ),
+          ]),
     );
   }
   }
@@ -43,57 +75,59 @@ class OptionsNotFound extends StatefulWidget {
   _OptionsNotFoundState createState() => _OptionsNotFoundState();
 }
 
-class _OptionsNotFoundState extends State<OptionsNotFound>{
-  optionWrong _option = optionWrong.QrNotWork;
+class _OptionsNotFoundState extends State<OptionsNotFound> {
+  optionWrong _option;
 
   Widget build(BuildContext context) {
     return Column(
-        children: <Widget>[
-          ListTile(
-            title: const Text("QR Code Does Not Work"),
-            leading: Radio(
-              value: optionWrong.QrNotWork,
-              groupValue: _option,
-              onChanged: (optionWrong value) {
-                setState(() {
-                  _option = value;
-                });
+      children: <Widget>[
+        ListTile(
+          title: const Text("QR Code Does Not Work"),
+          leading: Radio(
+            value: optionWrong.QrNotWork,
+            groupValue: _option,
+            onChanged: (optionWrong value) {
+              setState(() {
+                _option = value;
+                return subject1 = "QR Code Does Not Work";
+              });
             },
-            ),
           ),
-          ListTile(
-            title: const Text("Cache Not Found"),
-            leading: Radio(
-              value: optionWrong.NotFound,
-              groupValue: _option,
-              onChanged: (optionWrong value) {
-                setState(() {
-                  _option = value;
-                });
-              },
-            ),
+        ),
+        ListTile(
+          title: const Text("Cache Not Found"),
+          leading: Radio(
+            value: optionWrong.NotFound,
+            groupValue: _option,
+            onChanged: (optionWrong value) {
+              setState(() {
+                _option = value;
+                return subject1 = "Cache not found";
+              });
+            },
           ),
-          ListTile(
-            title: const Text("Other"),
-            leading: Radio(
-              value: optionWrong.Other,
-              groupValue: _option,
-              onChanged: (optionWrong value) {
-                setState(() {
-                  _option = value;
-                });
-              },
-            ),
+        ),
+        ListTile(
+          title: const Text("Other"),
+
+          leading: Radio(
+            value: optionWrong.Other,
+            groupValue: _option,
+            onChanged: (optionWrong value) {
+              setState(() {
+                _option = value;
+                return subject1 = "Other";
+              });
+            },
           ),
-              /**TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Issue",
-                ),
-              ),**/
-        ],
+        ),
+      ],
     );
   }
-
 }
+Email mailer  = Email(
+  subject: subject1,
+  body: body,
+  recipients: ['dhkreidler@gmail.com'],//temporary
+  isHTML: false,
+);
