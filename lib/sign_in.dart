0302f1ed63a9e4af0088645364049b176ccce2e9
Smekol <benjamin.mz.2003@gmail.com>
2020-07-30 16:09:19 -0400

    Removed print statements Removed tracer print statements

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -10,31 +10,28 @@ String imageUrl;
 
 Future<String> signInWithGoogle() async
 {
-  print("SIGN IN called");
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+  final GoogleSignInAuthentication googleSignInAuthentication =
+  await googleSignInAccount.authentication;
 
   final AuthCredential credential = GoogleAuthProvider.getCredential(
     accessToken: googleSignInAuthentication.accessToken,
     idToken: googleSignInAuthentication.idToken,
   );
-  print("AUTHORIZING");
+
   final AuthResult authResult = await _auth.signInWithCredential(credential);
   final FirebaseUser user = authResult.user;
+
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(user.uid == currentUser.uid);
+
   name= user.displayName;
   email=user.email;
   imageUrl=user.photoUrl;
 
-  print("DALTON LEIGHT WAS HERE");
-  print(name);
-  print(email);
-  print(imageUrl);
-  print("Stop 1");
-  assert(!user.isAnonymous);
-  assert(await user.getIdToken() != null);
-  print("Stop 2");
-  final FirebaseUser currentUser = await _auth.currentUser();
-  assert(user.uid == currentUser.uid);
   return 'signInWithGoogle succeeded: $user';
 }
 
