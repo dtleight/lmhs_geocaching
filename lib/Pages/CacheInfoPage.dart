a443d1a472c87245ffe491a27cc77aa95018166c
bzientara <55464616+bzientara@users.noreply.github.com>
2020-11-15 18:09:16 -0500

    First modification to improve UI. Added padding around different objects in the cacheinfopage. Note that a different padding was used for each child so that they are individually adjuatable. Also added two todos that need to be done later. Finally did some cleanup of the variables and organization.

--- lib/Pages/CacheInfoPage.dart
+++ lib/Pages/CacheInfoPage.dart
@@ -5,12 +5,10 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../Containers/CacheTrackingContainer.dart';
 import '../Objects/Cache.dart';
 
-class CacheInfoPage extends StatelessWidget
-{
+class CacheInfoPage extends StatelessWidget {
   GoogleMapController mapController;
 
-  void _onMapCreated(GoogleMapController controller)
-  {
+  void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   } // _onMapCreated
 
@@ -18,8 +16,8 @@ class CacheInfoPage extends StatelessWidget
   static GeoPoint _cacheLoc;
   static int _cacheID;
   Cache c;
-  CacheInfoPage(Cache cache)
-  {
+
+  CacheInfoPage(Cache cache) {
     this.c = cache;
     _cacheName = c.name;
     _cacheLoc = c.location;
@@ -27,27 +25,45 @@ class CacheInfoPage extends StatelessWidget
   } // CacheInfoPage Constructor
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     String _cacheLat = _cacheLoc.latitude.toString();
     String _cacheLng = _cacheLoc.longitude.toString();
     LatLng _cacheLatLng = new LatLng(_cacheLoc.latitude, _cacheLoc.longitude);
 
     return new Scaffold(
-        appBar: AppBar
-          (
-            title: Text(_cacheName + " Information"),
-            actions: <Widget>[IconButton( icon: Icon(Icons.my_location), onPressed: () { Navigator.push(context, new MaterialPageRoute(builder: (ctxt) => new CacheContainer(c))); },),],
-          ),
+        appBar: AppBar(
+          title: Text(_cacheName + " Information"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.my_location),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (ctxt) => new CacheContainer(c)));
+              },
+            ),
+          ],
+        ),
         body: ListView(
           children: <Widget>[
-            Text(
-              "Location: " + _cacheName + "\nLat: " + _cacheLat + " Long: " + _cacheLng,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
+            Padding(
+              padding: EdgeInsets.all(5),
+              child:
+              Text(
+                "Location: " +
+                    _cacheName +
+                    "\nLat: " +
+                    _cacheLat +
+                    " Long: " +
+                    _cacheLng,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
+
             SizedBox(
               height: 300,
               child: GoogleMap(
@@ -59,57 +75,68 @@ class CacheInfoPage extends StatelessWidget
               ),
             ),
             // History
-            Text(
-              "History",
-              style: TextStyle(
-                fontSize: 20,
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                "History",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            Text(c.description),
-            // Instructions
-            Text(
-              "Instructions",
-              style: TextStyle(
-                fontSize: 20,
+            Padding(
+              padding: EdgeInsets.all(5),
+              //todo: I commented out this variable to replace it with text for showcase purposes. Swap back later
+              child: //Text(c.description),
+              Text('Here is the history of the place.'),
+              // Instructions
+            ),
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                "Instructions",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
-            Text("These are the instructions to find Cache #1."),
-            // Comments
-            Text(
-              "Comments",
-              style: TextStyle(
-                fontSize: 20,
-              ),
+            Padding(
+              padding: EdgeInsets.all(5),
+              child:
+                  //todo the text description for how to find the cash should also be a variable
+                  Text("These are the instructions to find Cache #1."),
+            ),
+            Padding(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "Comments",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                )
+
+                // Comments
+
+                ),
+            Card(
+              child: Row(children: <Widget>[
+                Icon(Icons.person),
+                Text("This is one possible format for comments."),
+              ]),
             ),
             Card(
-              child:
-              Row(
-                  children: <Widget>[
-                    Icon(Icons.person),
-                    Text("This is one possible format for comments."),
-                  ]
-              ),
+              child: Row(children: <Widget>[
+                Icon(Icons.person),
+                Text("Here are two more,"),
+              ]),
             ),
             Card(
-              child:
-              Row(
-                  children: <Widget>[
-                    Icon(Icons.person),
-                    Text("Here are two more,"),
-                  ]
-              ),
-            ),
-            Card(
-              child:
-              Row(
-                  children: <Widget>[
-                    Icon(Icons.person),
-                    Text("so this can scroll."),
-                  ]
-              ),
+              child: Row(children: <Widget>[
+                Icon(Icons.person),
+                Text("so this can scroll."),
+              ]),
             ),
           ],
-        )
-    );
+        ));
   }
-}// CacheInfoPage Class
+} // CacheInfoPage Class
