2d4f6cf139a0ac8485f77a730f9ccc3a478d7412
mjmagee991 <54601318+mjmagee991@users.noreply.github.com>
2020-12-30 15:56:57 -0500

    Updated BadgeInfoPage.dart to look better and work with new theming system

--- lib/Pages/BadgeInfoPage.dart
+++ lib/Pages/BadgeInfoPage.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../Objects/Badge.dart';
+import '../Utilities/ColorTheme.dart';
 
 class BadgeInfoPage extends StatelessWidget {
-
-
   static bool _obtained;
   static String _badgeName;
   static int _badgeID;
@@ -15,7 +14,7 @@ class BadgeInfoPage extends StatelessWidget {
   static double progress;
 
   //Constructor
-  BadgeInfoPage(Badge B){
+  BadgeInfoPage(Badge B) {
     _obtained = B.isObtained;
     _badgeName = B.name;
     _badgeID = B.badgeID;
@@ -26,46 +25,80 @@ class BadgeInfoPage extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: AppBar (
-            title: Text(_badgeName),
-          ),
-        body: ListView(
-          children: <Widget>[
-          SizedBox(
-            width: double.infinity, // Fits the width of the screen
-            height: 250,
-            child: badgeImage,
-          ),
-            SizedBox(
-              height: 20,
+      appBar: AppBar(
+        title: Text("Badge Info"),
+      ),
+      backgroundColor: ColorTheme.backgroundColor,
+      body: Column(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(5, 5, 5, 10),
+          child: Text(
+            _badgeName,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: ColorTheme.textColor,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              description,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18,
+          ),
+        ),
+        SizedBox(
+          width: double.infinity, // Fits the width of the screen
+          height: 250,
+          child: badgeImage,
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(5, 10, 5, 20),
+          child: Text(
+            description,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: ColorTheme.textColor,
+              fontSize: 20,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+          child: Text(
+            "Badge Progress",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: ColorTheme.textColor,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: CircularProgressIndicator(
+                value: progress,
+                strokeWidth: 20,
+                valueColor: AlwaysStoppedAnimation(Colors.red[600]),
+                backgroundColor: ColorTheme.textColor,
               ),
             ),
-            SizedBox(
-              height: 30,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+          child: Text(
+            _obtained
+                ? "You earned this badge on: " + _unlockDate.toString()
+                : "You have not earned this badge.",
+            style: TextStyle(
+              color: ColorTheme.textColor,
+              fontSize: 18,
             ),
-            Text(_obtained? "You earned this badge on: " + _unlockDate.toString():"You have not earned this badge.",style: TextStyle(fontSize: 18),),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Badge Progress",
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            ),
-            LinearProgressIndicator(
-              value: progress,
-            ),
-        ]),
+          ),
+        ),
+      ]),
     );
   }
 }
